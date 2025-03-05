a20b0d17ccd7dc7eaf541dceb547522e50493707
shoaibasad <sunnymalik6772@gmail.com>
2025-03-05 16:15:27 +0500

    ds listing fix bug

--- resources/views/pages/ds160/listing.blade.php
+++ resources/views/pages/ds160/listing.blade.php
@@ -56,7 +56,8 @@
 
                 <tr style="font-size: small;">
                   <td>{{ $loop->iteration ?? '' }}</td>
-                  <td>{{ $ds->client->name ? $ds->client->name .'~'. $ds->client->sur_name : 'N/A' }}</td>
+                  
+                  <td>{{ ($ds->client->name ?? 'N/A') . (isset($ds->client->sur_name) ? ' ~ ' . $ds->client->sur_name : '') }}</td>
                   <td>{{ $ds->category->name ?? 'N/A' }}</td>
                   <td>{{ $ds->ds160 ?? '' }}</td>
                   <td>{{ $ds->revised_ds160 ?? '' }}</td>
