25aa3860d14005236f49ac485d9d55f193301920
memoonabibi098 <memoonaworkmail@gmail.com>
2025-12-18 09:49:28 +0500

    18-12-2025
    application module coulmn rename

--- resources/views/pages/Application/listing.blade.php
+++ resources/views/pages/Application/listing.blade.php
@@ -135,7 +135,7 @@
                   <th>Contact No</th>
                   <th>Submission Date</th>
                   <th>Visa Status</th>
-                  <th>Status</th>
+                  <th>Internal Status</th>
                   <th>Action</th>
                 </tr>
               </thead>
